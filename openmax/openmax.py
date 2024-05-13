d01ce9c6f0ea789b8a35f8e9392db2442962d1d0
Requiem <public_receiver035@pm.me>
2024-05-13 23:11:22 +0700

    update

--- openmax/openmax.py
+++ openmax/openmax.py
@@ -77,7 +77,7 @@ def recalibrate_scores(weibull_model, activation_vector):
     alpha_weights = [
         ((ALPHA_RANK+1) - i) / float(ALPHA_RANK) for i in range(1, ALPHA_RANK+1)
     ]
-    print(f'alpha_weights: {alpha_weights}')
+    # print(f'alpha_weights: {alpha_weights}')
     ranked_alpha = np.zeros_like(activation_vector)
     for i in range(len(alpha_weights)):
         ranked_alpha[ranked_list[i]] = alpha_weights[i]
@@ -90,7 +90,7 @@ def recalibrate_scores(weibull_model, activation_vector):
         weibull = query_weibull(label, weibull_model)
         av_distance = compute_distance(weibull['mean'], activation_vector)
         wscore = weibull['weibull_model'].w_score(av_distance)
-        print(f'wscore_{label}: {wscore}')
+        # print(f'wscore_{label}: {wscore}')
         modified_score = activation_vector[label] * \
             (1 - wscore*ranked_alpha[label])
         openmax_scores += [modified_score]
@@ -98,10 +98,10 @@ def recalibrate_scores(weibull_model, activation_vector):
 
     openmax_scores = np.asarray(openmax_scores)
     openmax_scores_u = np.asarray(openmax_scores_u)
-    print(f'openmax_score: {openmax_scores}')
-    print(f'openmax_score_u: {openmax_scores_u}')
-    print(f'sum openmax_score_u: {np.sum(openmax_scores_u)}')
-    print('-' * 80)
+    # print(f'openmax_score: {openmax_scores}')
+    # print(f'openmax_score_u: {openmax_scores_u}')
+    # print(f'sum openmax_score_u: {np.sum(openmax_scores_u)}')
+    # print('-' * 80)
 
     openmax_prob, prob_u = compute_openmax_probability(
         openmax_scores, openmax_scores_u)
